9eb2c5b215987b8334720abea83aff65034f5cdc
洁神 <1041749499@qq.com>
2023-06-07 18:21:06 +0800

    22

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import Providers from './Providers'
-import './utils/auto-update'  // 检测自动更新
+import './utils/auto-update'  
 import './index.css'
 import './tailwind.css'
 
